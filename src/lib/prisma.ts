b2f8a6f1bf908119e473830046dbd6814acfa341
Llane4 <llane4@hotmail.es>
2026-03-25 21:39:52 -0300

    feat: initial commit

---
+++ src/lib/prisma.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client'
+
+// Singleton para desarrollo (evita múltiples instancias con hot reload)
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
